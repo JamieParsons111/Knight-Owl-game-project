28285799ea0bfcafe9e4b5a9617bfe8f698651f0
Jamie Parsons <jamieljparsons111@gmail.com>
2023-07-21 18:08:17 +0100

    Knight Owl 0.12 Created a moving rectangle on a black background

--- main.py
+++ main.py
@@ -3,15 +3,53 @@ import pygame
 pygame.init()
 
 #defining size of game window
-windowsSize = pygame.display.set_mode((800,600)) 
-pygame.display.set_caption("Jamie game")
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-#defining font attributes
-myFont = pygame.font.SysFont("Arial", 90)
-helloWorld = myFont.render("Hello World", 1, (255, 0, 255), (255, 255, 255))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-while 1:
+'''Defining the variable player as a rectangle (Rect), taking in variables:
+# X & Y coordinates, and height and width of rectangle
+''' 
+player = pygame.Rect((300, 250, 50, 50))
+
+
+# Just for quitting out the game
+run = True
+''' 
+pygame.draw.rect(screen,(100, 200,100))
+Draws out the rectangle on the screen, 
+Next Parenthesis is the RBG Colours of the rectangle itself
+'''
+while run:
+
+    #Screen background colour
+    screen.fill((0,0,0))
+
+    #This is the player 
+    pygame.draw.rect(screen,(100, 200,100),player)
+
+    key = pygame.key.get_pressed()
+    #Movement
+    # Move left key
+    if key[pygame.K_a] == True:
+        # move_ip = Move in place
+        player.move_ip(-1, 0)
+    # Move Right key
+    elif key[pygame.K_d] == True:
+        player.move_ip(1,0)
+
+    # Move Up key
+    elif key[pygame.K_w] == True:
+        player.move_ip(0,-1)
+
+    # Move down key
+    elif key[pygame.K_s] == True:
+        player.move_ip(0,1)
+
     for event in pygame.event.get():
-        if event.type==pygame.QUIT: sys.exit()
-    windowsSize.blit(helloWorld, (0, 0))
+        if event.type== pygame.QUIT:
+            run = False
     pygame.display.update()
+
+pygame.quit()
