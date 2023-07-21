dbb64b597ef2565140c817e8237938436b0631a3
Jamie Parsons <jamieljparsons111@gmail.com>
2023-07-21 20:21:00 +0100

    Knight Owl 0.1.3
    -Importing my own custom sprite for the main player character
    - Made the image be moveable now replacing the moveable rectangle
    - Updating the name (caption) of the game to Knight Owl

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame, sys, os, random, time
+from pygame.locals import * #Gives acess to constants etc. 
 
 pygame.init()
 
@@ -7,13 +8,20 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Knight Owl")
+
 
 '''Defining the variable player as a rectangle (Rect), taking in variables:
 # X & Y coordinates, and height and width of rectangle
 ''' 
-player = pygame.Rect((300, 250, 50, 50))
 
-
+x = 200
+y = 200
+scale = 2 
+player = pygame.image.load('images/player.png')
+player = pygame.transform.scale(player,(player.get_width()* 3, player.get_height()* scale))
+rect = player.get_rect()
+rect.center = (x,y)
 # Just for quitting out the game
 run = True
 ''' 
@@ -27,25 +35,25 @@ while run:
     screen.fill((0,0,0))
 
     #This is the player 
-    pygame.draw.rect(screen,(100, 200,100),player)
-
+    #pygame.draw.rect(screen,(100, 200,100),player)
+    screen.blit(player, rect)
     key = pygame.key.get_pressed()
     #Movement
     # Move left key
     if key[pygame.K_a] == True:
         # move_ip = Move in place
-        player.move_ip(-1, 0)
+        rect.move_ip(-1, 0)
     # Move Right key
     elif key[pygame.K_d] == True:
-        player.move_ip(1,0)
+        rect.move_ip(1,0)
 
     # Move Up key
     elif key[pygame.K_w] == True:
-        player.move_ip(0,-1)
+        rect.move_ip(0,-1)
 
     # Move down key
     elif key[pygame.K_s] == True:
-        player.move_ip(0,1)
+        rect.move_ip(0,1)
 
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
